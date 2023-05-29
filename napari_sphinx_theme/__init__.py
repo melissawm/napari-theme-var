6a404f53714964a20aef575203e715a9a7635db6
melissawm <melissawm@gmail.com>
2023-05-29 11:11:18 -0300

    Add basic styling

--- napari_sphinx_theme/__init__.py
+++ napari_sphinx_theme/__init__.py
@@ -1,6 +1,33 @@
+import os
 from pathlib import Path
 
 __version__ = "0.0.1dev"
+
+
+def update_templates(app, pagename, templatename, context, doctree):
+    """Update template names for page build."""
+    template_sections = [
+        "theme_navbar_start",
+        "theme_navbar_center",
+        "theme_navbar_end",
+        "theme_footer_items",
+        "theme_page_sidebar_items",
+        "theme_left_sidebar_end",
+        "sidebars",
+    ]
+
+    for section in template_sections:
+        if context.get(section):
+            # Break apart `,` separated strings so we can use , in the defaults
+            if isinstance(context.get(section), str):
+                context[section] = [
+                    ii.strip() for ii in context.get(section).split(",")
+                ]
+
+            # Add `.html` to templates with no suffix
+            for ii, template in enumerate(context.get(section)):
+                if not os.path.splitext(template)[1]:
+                    context[section][ii] = template + ".html"
 
 
 def set_config_defaults(app):
@@ -49,5 +76,10 @@ def setup(app):
     # app.config.templates_path.append(str(here / "components"))
     app.add_html_theme("napari_sphinx_theme", str(here))
     app.connect("builder-inited", set_config_defaults)
-    #app.connect("html-page-context", setup_html_page_context)
+    app.connect("html-page-context", update_templates)
+
+    # Include templates for sidebar
+    print(f"{here=}")
+    app.config.templates_path.append(str(here / "_templates"))
+
     return {'version': __version__, 'parallel_read_safe': True}
