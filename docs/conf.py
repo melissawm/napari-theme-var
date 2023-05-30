9237d897dafc4033974581519c9bcb2fcc1ae667
melissawm <melissawm@gmail.com>
2023-05-30 17:51:17 -0300

    Adding styling

--- docs/conf.py
+++ docs/conf.py
@@ -1,12 +1,11 @@
 import os
 import time
 import datetime
-import napari  # noqa: F401
+from napari_sphinx_theme import __version__
 # Configuration file for the Sphinx documentation builder for
 # matplotlib projects.
 
-# Release mode enables optimizations and other related options.
-is_release_build = tags.has('release')  # noqa
+version = __version__
 
 # Parse year using SOURCE_DATE_EPOCH, falling back to current time.
 # https://reproducible-builds.org/specs/source-date-epoch/
@@ -18,21 +17,15 @@ build_date = datetime.datetime.utcfromtimestamp(
 
 project = "napari Sphinx Theme"
 copyright = (
-    f"2023 - {build_date.year} The napari team"
+    f"2023 - {build_date.year} napari Community"
 )
-author = "napari team"
+author = "napari Community"
 
 
 # -- General configuration ---------------------------------------------------
 
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
-# extensions = [
-#     'matplotlib.sphinxext.plot_directive',
-# ]
-
 # Add any paths that contain templates here, relative to this directory.
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -42,6 +35,17 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # -- Options for HTML output -------------------------------------------------
 
 html_theme = "napari_sphinx_theme"
+
+# Define the json_url for our version switcher.
+json_url = "https://napari.org/napari-sphinx-theme/_static/switcher.json"
+if "dev" in version:
+    version_match = "latest"
+    # We want to keep the relative reference if we are in dev mode
+    # but we want the whole url if we are effectively in a released version
+    json_url = "_static/switcher.json"
+else:
+    version_match = version
+
 html_theme_options = {
     # logo is installed by mpl-sphinx-theme as:
     # "logo": {"link": "https://matplotlib.org/stable/",
@@ -50,11 +54,26 @@ html_theme_options = {
     # if this default is OK, then no need to modify "logo"
     # collapse_navigation in pydata-sphinx-theme is slow, so skipped for local
     # and CI builds https://github.com/pydata/pydata-sphinx-theme/pull/386
-    "collapse_navigation": not is_release_build,
+    "show_toc_level": 1,
     "show_prev_next": False,
+    "navbar_end": ["version-switcher"],
+    # "left_sidebar_end": ["custom-template.html", "sidebar-ethical-ads.html"],
+    # "footer_items": ['navbar-version', 'napari-footer-links', 'copyright'],
+    "switcher": {
+        "json_url": "https://napari.org/napari-sphinx-theme/_static/switcher.json",
+        "version_match": version_match,
+    },
 }
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 # html_static_path = ["_static"]
+
+html_sidebars = {
+    "**": [
+        "search-field",
+        "sidebar-nav-bs",
+    ],
+}
+
